8b79483fd7ce4f470403a397d05322f972cbf1d2
natandreli <natalia.garcia9@udea.edu.co>
2025-12-04 09:07:12 -0500

    refactor: :fire: Delete unused services folder

--- app/services/__init__.py
+++
@@ -1,4 +0,0 @@
-from app.services.instances.instance_manager import instance_manager
-from app.services.solver import solver_service
-
-__all__ = ["instance_manager", "solver_service"]
--- app/services/solver/__init__.py
+++
@@ -1,3 +0,0 @@
-from app.services.solver.solver_service import solver_service
-
-__all__ = ["solver_service"]
